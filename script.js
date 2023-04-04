798ceb7f43147fd1dc83bf995efc85421b0e1cce
daveyhert <daveyhert@gmail.com>
2023-04-04 23:02:04 +0100

    test

--- script.js
+++ script.js
@@ -6,19 +6,26 @@ const tips = [];
 const totals = [];
 
 function calcTip(bill) {
-  let tip = 0;
-
-  if (bill >= 50 && bill <= 300) {
-    tip = (15 / 100) * bill;
-  } else {
-    tip = (20 / 100) * bill;
-  }
-  return tip;
-}
-
-function calTip(bill) {
   let tip = bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill;
-
   return tip;
 }
-ßß;
+function checkOut() {
+  for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+  }
+}
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+checkOut();
+console.log(tips);
+console.log(totals);
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
