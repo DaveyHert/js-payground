88567c1e782cc635624117cb2ac526638fb38697
daveyhert <daveyhert@gmail.com>
2023-04-27 17:32:02 +0100

    Test

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-"use strict";
+// console.log(a);
+console.log(b);
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 110, 86, 52];
-
-const tips = [];
-const totals = [];
-
-function calcTip(bill) {
-  let tip = bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill;
-  return tip;
-}
-function checkOut() {
-  for (let i = 0; i < bills.length; i++) {
-    tips.push(calcTip(bills[i]));
-    totals.push(bills[i] + tips[i]);
-  }
-}
-
-function calcAverage(arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-
-checkOut();
-console.log(tips);
-console.log(totals);
-console.log(calcAverage(tips));
-console.log(calcAverage(totals));
+const a = 'Ant';
+let b = 'Boat';
+rr;
